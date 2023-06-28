ad1bfe9d64548177c16c39fa58d5a9a374607875
Thomas Pasfield <111624637+tpasfieldERAU@users.noreply.github.com>
2023-06-28 15:11:51 -0400

    Better optimize GCV parameter selection

--- GCV.py
+++ GCV.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.fft import fft
-from math import sqrt
+from math import sqrt, log10
 import matplotlib.pyplot as plt
 
 debug = False
@@ -9,36 +9,63 @@ def dft(x):
     m, n = np.shape(x)
     return fft(x) / (sqrt(m)*sqrt(n))
 
-def toggleDebug():
+def toggleDebug(default=0):
     global debug
-    debug = not debug
+    if default != 0:
+        if default > 0:
+            debug = True
+        else:
+            debug = False
+    else:
+        debug = not debug
 
 def run_gcv(A, b, func, n):
+    lim = n
     d_hat = dft(b)
-    alphavec = np.logspace(-6, 4, n)
     n = np.shape(b)[0]
-
-    GCV = np.zeros(np.shape(alphavec))
-
     x_re = np.zeros(np.shape(b))
-    ref = 100
-    for i in range(max(np.shape(alphavec))):
-        alpha = alphavec[i]
-        A_hat = func(b, A, alpha)
-        resid = abs(np.multiply((A_hat - 1), d_hat))
-        rms_resid = sum(np.square(resid)) / n
-        trace = sum(A_hat)
-        Vdenom = (1 - trace/n)**2
+    GCV = []
 
-        GCV[i] = rms_resid / Vdenom
-        if GCV[i] < ref:
-            ref = GCV[i]
-            x_re = A_hat
+
+    ref = 100
+    j = 0
+    thresh = 100
+    loc = -1
+    low = -10
+    high = 10
+    out = []
+
+
+    while j < lim:
+        if j != 0:
+            span = high - low
+            low = loc - span/4
+            high = loc + span/4
+        alphavec = np.logspace(low, high, 10)
+
+        for i in range(max(np.shape(alphavec))):
+            alpha = alphavec[i]
+            A_hat = func(b, A, alpha)
+            resid = abs(np.multiply((A_hat - 1), d_hat))
+            rms_resid = sum(np.square(resid)) / n
+            trace = sum(A_hat)
+            Vdenom = (1 - trace/n)**2
+
+            GCV.append(rms_resid / Vdenom)
+            if GCV[-1] < ref:
+                ref = GCV[-1]
+                x_re = A_hat
+                loc = log10(alpha)
+            out.append([alpha, GCV[-1]])
+        j += 1
 
     if debug:
-        plt.loglog(alphavec, GCV, '-.')
+        plt.scatter(*zip(*out), 12)
+        plt.xscale("log")
+        plt.yscale("log")
         plt.xlabel(r'Regularization Parameter \alpha')
         plt.title(r'GCV(\alpha)')
         plt.show()
 
-    return x_re, min(GCV)
+
+    return x_re, out
